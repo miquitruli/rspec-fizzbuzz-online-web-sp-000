1140759b9321dd315a9c9959f19e1714e3bcf751
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-23 00:35:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0# if the number int is divisible by 3
+    return "FizzBuzz"
+  elsif int % 3 == 0
     return "Fizz"
-elsif int % 5 == 0 # if the number int is divisible by 3
-  return "Buzz"
-elsif int % 3 == 0
-  int % 5 == 0
-  return "FizzBuzz"
+  elsif int % 5 == 0 # if the number int is divisible by 3
+    return "Buzz"
   end
 end
