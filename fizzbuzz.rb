04c5016a4e20c48253fafaa416c938171b166709
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-23 00:22:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,9 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+    return "Fizz"
+  end
+elsif int % 5 == 0 # if the number int is divisible by 3
+  return "Buzz"
   end
 end
